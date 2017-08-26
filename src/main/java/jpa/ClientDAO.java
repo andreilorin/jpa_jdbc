b132fb1149287b210641e632f953f438944a3f81
andrei lorin <andreilorin@gmail.com>
2017-08-26 22:06:15 +0100

    changed main to implement dao patter

--- src/main/java/jpa/ClientDAO.java
+++ src/main/java/jpa/ClientDAO.java
@@ -1,4 +1,13 @@
 package jpa;
 
-public class ClientDAO {
+import java.util.List;
+
+public abstract class ClientDAO {
+
+    public abstract void listClients();
+    public abstract int addClient(String name, String address1, String address2);
+    public abstract void updateClient(int employeeId, String name );
+    public abstract void deleteClient(int employeeId);
+
+
 }
